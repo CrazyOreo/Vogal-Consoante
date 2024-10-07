a4507514ab1c7c44c9a9745ea6e3b615b0ea0f94
CrazyOreo <pedromacegosanovais@gmail.com>
2024-10-07 08:55:54 -0300

    Commit

---
+++ Sistema de Vogal ou consoate.py
@@ -0,0 +1,7 @@
+valor=input("Por favor, insira seu sexo\n")
+
+if valor == "A" or "E" or "I" or "O" or "U" or "a" or "e" or "i" or "o" or "u":
+    print("Vogal")
+else:
+    print("Consoante")
+    
